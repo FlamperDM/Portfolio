67d43f8a92d6326dd5eae70a4f4354f46f5492f9
br-flampeyeiry <flampeyeiry@bitreel.com>
2021-11-10 13:40:34 -0400

    react init

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+
+
+
+This project is based on the [Bootstrap Agency Template](https://startbootstrap.com/themes/agency/) adapted and refactored using React JS and some custom style.
+
+
+
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import './asset/css/bootstrap.min.css'
+import './asset/css/fonts.css'
+import './asset/css/base.css'
+
+import './asset/css/section/navbar.css'
+import './asset/css/section/header.css'
+import './asset/css/section/portfolio.css'
+import './asset/css/section/about.css'
+import './asset/css/section/experience.css'
+import './asset/css/section/contact.css'
+import './asset/css/section/footer.css'
+
+import './asset/css/agency.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
